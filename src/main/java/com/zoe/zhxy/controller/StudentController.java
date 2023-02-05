06123abd0314c35397732e9b49d4c9c4fd34e9b3
zoe <zoeeying@gmail.com>
2023-02-05 22:40:00 +0800

    feat: 学生接口完善

--- src/main/java/com/zoe/zhxy/controller/StudentController.java
+++ src/main/java/com/zoe/zhxy/controller/StudentController.java
@@ -4,14 +4,15 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zoe.zhxy.pojo.Student;
 import com.zoe.zhxy.service.StudentService;
+import com.zoe.zhxy.util.MD5;
 import com.zoe.zhxy.util.Result;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sms/studentController")
@@ -19,6 +20,29 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
+
+    @ApiOperation("删除Student")
+    @DeleteMapping("/delStudentById")
+    public Result delStudentById(
+            @ApiParam("要删除的Student的ID集合") @RequestBody List<Integer> ids
+    ) {
+        studentService.removeByIds(ids);
+        return Result.ok();
+    }
+
+    @ApiOperation("新增或修改Student")
+    @PostMapping("/addOrUpdateStudent")
+    public Result addOrUpdateStudent(
+            @ApiParam("Student对象") @RequestBody Student student
+    ) {
+        Integer id = student.getId();
+        // 新增的Student，需要对密码进行加密
+        if (null == id || 0 == id) {
+            student.setPassword(MD5.encrypt(student.getPassword()));
+        }
+        studentService.saveOrUpdate(student);
+        return Result.ok();
+    }
 
     @ApiOperation("分页查询Student")
     @GetMapping("/getStudentByOpr/{current}/{pageSize}")
